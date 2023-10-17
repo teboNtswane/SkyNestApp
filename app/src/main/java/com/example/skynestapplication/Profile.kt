1251d70e6cee1862b0a5a791924b5290f0e4306e
teboNtswane <tebo.ntswane@gmail.com>
2023-10-17 16:20:09 +0200

    > added google json file > added navigation which works > added profile screen which i'm still busy with

--- app/src/main/java/com/example/skynestapplication/Profile.kt
+++ app/src/main/java/com/example/skynestapplication/Profile.kt
@@ -3,20 +3,49 @@ package com.example.skynestapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.skynestapplication.databinding.ActivityHomeDashboardBinding
+import android.widget.EditText
+import android.widget.TextView
 import com.example.skynestapplication.databinding.ActivityProfileBinding
+import com.example.skynestapplication.databinding.ActivitySignUpBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class HomeDashboard : AppCompatActivity() {
+class Profile : AppCompatActivity() {
 
-    private lateinit var binding: ActivityHomeDashboardBinding
+    private lateinit var binding: ActivityProfileBinding
+
+    private lateinit var name: EditText
+
+    private lateinit var surname: EditText
+
+    private lateinit var username: TextView
+    private lateinit var email: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityHomeDashboardBinding.inflate(layoutInflater)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         navigate()
-    }
 
+        var nameDisplay = ""
+        var emailDisplay = ""
+
+        name = findViewById(R.id.tv_firstname)
+        surname = findViewById(R.id.tv_lastname)
+        username = findViewById(R.id.tv_name)
+        email = findViewById(R.id.tv_email)
+
+        nameDisplay = "$name $surname"
+        emailDisplay = "$email"
+
+        username.text = nameDisplay
+        email.text = emailDisplay
+
+        //Directs user to Login screen
+        binding.btnSignOut.setOnClickListener {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
+    }
 
     //--------------------------------------------------------------------------------------------------
     //Reference: Mafia Codes
@@ -48,5 +77,4 @@ class HomeDashboard : AppCompatActivity() {
         }
 
     }
-
 }
